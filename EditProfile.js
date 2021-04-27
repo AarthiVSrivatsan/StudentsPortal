97ca93d581dfd87cc4cb08d19cbb81e8f08e3acd
AarthiVSrivatsan <76556758+AarthiVSrivatsan@users.noreply.github.com>
2021-04-27 16:07:15 +0530

    Bug Fix - added designation

--- EditProfile.js
+++ EditProfile.js
@@ -4,6 +4,7 @@ class EditProfile{
     var gradyear = document.getElementById("gradyear").value;
     var batch = document.getElementById("batch").value;
     var streamzs = document.getElementById("streamzs").value;
+    var designation = document.getElementById("designation").value;
     var userid = document.getElementById("userid").value;
 
     var params = {};
@@ -11,14 +12,15 @@ class EditProfile{
     gradyear && (params.gradyear = gradyear);
     batch && (params.batch = batch);
     streamzs && (params.streamzs = streamzs);
+    designation && (params.designation = designation);
     userid && (params.user = userid);
 
     $DX.get({
        url: "/fsdb/function/editProfile",
        params: params,
        handler: function(response) {
-         console.log(this.responseText);
-         if(this.responseText == "Success") {
+         console.log(response);
+         if(response == "Success") {
            window.location.href="/profileDetails?studentId="+userid;
          }
        }
