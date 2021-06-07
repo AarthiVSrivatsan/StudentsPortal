f6ac1eb20cd4951e1557a1864d6e1f98fb7522a1
AarthiVSrivatsan <76556758+AarthiVSrivatsan@users.noreply.github.com>
2021-06-07 14:14:18 +0530

    Update ProjectDetails.js

--- ProjectDetails.js
+++ ProjectDetails.js
@@ -5,3 +5,6 @@ var ProjectDetails = function() {}
 ProjectDetails.openProjectPage = function(options) {
     window.location.href="/profileDetails?studentId="+options.params["student-id"];
   }
+ProjectDetails.openFacultyDetails = function(options) {
+  window.location.href = "/faculty-details?userid="+options.params["user-id"]
+}
